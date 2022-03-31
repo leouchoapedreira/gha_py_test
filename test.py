0868803784b75f042bb4e159883bbe6ab96fb487
Leonardo Uchoa <leonardo.pedreira.github@samplemed.com.br>
2022-03-31 16:59:12 -0300

    os.getenv to os.environ

--- test.py
+++ test.py
@@ -6,5 +6,12 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 
 TEST = os.getenv("TEST")
 
+if TEST:
+    print(f"TEST should be {TEST} in local")
+else:
+    TEST = os.environ["TEST"]
+    print(f"TEST should be {TEST} in gha")
+
+
 print(TEST)
 
